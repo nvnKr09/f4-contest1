66efc99336212f5ea977c3100be09bc92870ff17
Naveen Kumar <nvnkr09@gmail.com>
2023-12-16 18:11:17 +0530

    new changes

--- src/Components/signUp/signUp.jsx
+++ src/Components/signUp/signUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./signUp.css";
 
-const signUp = () => {
+const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [cnfPassword, setCnfPassword] = useState("");
@@ -119,4 +119,4 @@ const signUp = () => {
   );
 };
 
-export default signUp;
+export default SignUp;
